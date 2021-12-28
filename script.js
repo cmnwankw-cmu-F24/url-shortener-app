f3b0ba8998ec05877ba67c4760ee1c7417a58a94
Minard-NG <michael4sure20@gmail.com>
2021-12-28 17:57:37 +0100

    completed mobile nav bar

---
+++ script.js
@@ -0,0 +1,35 @@
+//mobile drop down animation
+let harm_btn = document.querySelector('.harmburger-btn');
+let spanOne = document.querySelector('.harmburger-btn > span:first-child');
+let spanTwo = document.querySelector('.harmburger-btn > span:nth-child(2)');
+let spanThree = document.querySelector('.harmburger-btn > span:last-child');
+let dropDown = document.querySelector('#mobile__drop-down');
+
+console.log(dropDown.classList.length === 0)
+harm_btn.addEventListener('click', (evt) => {
+    if (dropDown.classList.length === 0 || dropDown.classList.contains('dropDownReturn')) {
+        //remove existing return state animation class, if present
+        spanOne.classList.remove('spanOneReturn');
+        spanTwo.classList.remove('spanTwoReturn');
+        spanThree.classList.remove('spanThreeReturn');
+        dropDown.classList.remove('dropDownReturn');
+
+        //add end state animation classes
+        spanOne.classList.add('spanOneEnd');
+        spanTwo.classList.add('spanTwoEnd');
+        spanThree.classList.add('spanThreeEnd');
+        dropDown.classList.add('dropDownEnd');
+    } else {
+        //remove existing end state animation class, if present
+        spanOne.classList.remove('spanOneEnd');
+        spanTwo.classList.remove('spanTwoEnd');
+        spanThree.classList.remove('spanThreeEnd');
+        dropDown.classList.remove('dropDownEnd');
+
+        //add return state animation classes
+        spanOne.classList.add('spanOneReturn');
+        spanTwo.classList.add('spanTwoReturn');
+        spanThree.classList.add('spanThreeReturn');
+        dropDown.classList.add('dropDownReturn');
+    }
+});
